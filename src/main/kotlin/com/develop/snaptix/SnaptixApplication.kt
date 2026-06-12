9e0b39cf5314701f773254525cd15209eeae6fec
octuniv <hws090911@gmail.com>
2026-06-12 09:28:45 +0900

    initial commit

---
+++ src/main/kotlin/com/develop/snaptix/SnaptixApplication.kt
@@ -0,0 +1,11 @@
+package com.develop.snaptix
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SnaptixApplication
+
+fun main(args: Array<String>) {
+    runApplication<SnaptixApplication>(*args)
+}
---
+++ src/test/kotlin/com/develop/snaptix/SnaptixApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.develop.snaptix
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SnaptixApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
