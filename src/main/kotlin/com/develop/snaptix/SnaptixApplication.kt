5624332a1ec50846fbb2d5ab529bcfcfb95722e4
csgangsai06512-ue <csgangsai06512@gmail.com>
2026-06-19 15:40:00 +0900

    CheckinTicketValidation_260619

--- src/main/kotlin/com/develop/snaptix/SnaptixApplication.kt
+++ src/main/kotlin/com/develop/snaptix/SnaptixApplication.kt
@@ -1,9 +1,13 @@
 package com.develop.snaptix
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
+@EnableScheduling
 class SnaptixApplication
 
 fun main(args: Array<String>) {
