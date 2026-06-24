11be84c1e59882fc25d7f77fa60c62a8271d97b0
octuniv <hws090911@gmail.com>
2026-06-24 15:26:36 +0900

    refactor: 서킷브레이커 일원화 및 503 매핑 (#16)
    - GlobalExceptionHandler에 RedisUnavailableException 전용 핸들러 추가
      → BusinessException 핸들러보다 타입 구체성 우선, 서킷 OPEN 시 일관된 503 반환
    - OrderOwnerStore: @RedisCircuitBreaker AOP 제거, ResilientRedisExecutor.execute() 전환
      → RedisAction.OWNERSHIP 사용 (신규 enum 값 불필요)
    - RedisOperationLoggingAspect / @RedisOperation 제거
      → 로깅은 ResilientRedisExecutor → RedisActionLogger 일원화
    - RedisCircuitBreakerAspect / @RedisCircuitBreaker 제거
      → 서킷 적용 지점 ResilientRedisExecutor 단일화
    - RedisCircuitBreakerEventListener 유지 (Slack 알림, out of scope)

--- src/main/kotlin/com/develop/snaptix/global/aop/annotation/RedisOperation.kt
+++
@@ -1,9 +0,0 @@
-package com.develop.snaptix.global.aop.annotation
-
-import com.develop.snaptix.global.aop.type.RedisAction
-
-@Target(AnnotationTarget.FUNCTION)
-@Retention(AnnotationRetention.RUNTIME)
-annotation class RedisOperation(
-    val action: RedisAction,
-)
