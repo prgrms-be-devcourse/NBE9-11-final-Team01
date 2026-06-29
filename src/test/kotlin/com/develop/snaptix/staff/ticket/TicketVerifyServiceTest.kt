503365b9b56fc1781dcbad31220f47557bed9162
csgangsai06512-ue <csgangsai06512@gmail.com>
2026-06-29 15:47:46 +0900

    PR-03 Tests

---
+++ src/test/kotlin/com/develop/snaptix/staff/ticket/TicketVerifyServiceTest.kt
@@ -0,0 +1,42 @@
+package com.develop.snaptix.staff.ticket
+
+import org.junit.jupiter.api.Test
+
+/**
+ * PR-03 Verify API Service Tests
+ *
+ * NOTE:
+ * Adjust imports/package names if your branch differs.
+ * Generated from the uploaded PR-03 implementation discussion.
+ */
+class TicketVerifyServiceTest {
+    @Test
+    fun verify_event_not_found() {
+        // TODO
+    }
+
+    @Test
+    fun verify_ticket_not_found() {
+        // TODO
+    }
+
+    @Test
+    fun verify_reservation_not_found() {
+        // TODO
+    }
+
+    @Test
+    fun verify_event_mismatch() {
+        // TODO
+    }
+
+    @Test
+    fun verify_ticket_already_used() {
+        // TODO
+    }
+
+    @Test
+    fun verify_success() {
+        // TODO
+    }
+}
